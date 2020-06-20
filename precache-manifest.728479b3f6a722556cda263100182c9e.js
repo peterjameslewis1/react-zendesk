2af842547c20d358fa4979054aeb994dde9d49f6
Peter <peterjameslewis4@hotmail.com>
2020-06-20 15:53:11 +0100

    Updates

---
+++ precache-manifest.728479b3f6a722556cda263100182c9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb61f089799380cd6cb1922ddb2c0bf",
+    "url": "/react-zendesk/index.html"
+  },
+  {
+    "revision": "e2f6da7a1c8e52464887",
+    "url": "/react-zendesk/static/css/main.07575860.chunk.css"
+  },
+  {
+    "revision": "6b02693efbc0143da891",
+    "url": "/react-zendesk/static/js/2.a63afae8.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/react-zendesk/static/js/2.a63afae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f6da7a1c8e52464887",
+    "url": "/react-zendesk/static/js/main.41f6080f.chunk.js"
+  },
+  {
+    "revision": "bdc5e542f12968d92455",
+    "url": "/react-zendesk/static/js/runtime-main.4c334f90.js"
+  },
+  {
+    "revision": "9dc12e7119b2d020f6d915fe249bd81a",
+    "url": "/react-zendesk/static/media/Section1.9dc12e71.less"
+  },
+  {
+    "revision": "6e9c47d8b0fc2803db746f0dca69d25a",
+    "url": "/react-zendesk/static/media/Section2.6e9c47d8.less"
+  }
+]);
