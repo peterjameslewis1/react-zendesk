0427336c5c26505c24e46612b325b6c1956e5b5c
peterjameslewis1 <51764142+peterjameslewis1@users.noreply.github.com>
2020-07-16 14:38:56 +0100

    Update CustomerExperience.js

--- src/CustomerExp/CustomerExperience.js
+++ src/CustomerExp/CustomerExperience.js
@@ -4,7 +4,7 @@ import { useInView } from 'react-intersection-observer'
 
 const CustomerExperience = () => {
     const [ref, inView] = useInView({
-        threshold: 0.5,
+        rootMargin: '-500px 0px',
         triggerOnce: true,
       })
 
