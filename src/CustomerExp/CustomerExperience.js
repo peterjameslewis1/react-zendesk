8d66eca7b0754ef38766dc99c9553a7c4cd19d8f
Peter Lewis <peterjameslewis4@hotmail.com>
2020-07-16 14:57:30 +0100

    Intersection Observer fix

--- src/CustomerExp/CustomerExperience.js
+++ src/CustomerExp/CustomerExperience.js
@@ -4,17 +4,17 @@ import { useInView } from 'react-intersection-observer'
 
 const CustomerExperience = () => {
     const [ref, inView] = useInView({
-        rootMargin: '-500px 0px',
+        threshold: 0.5,
         triggerOnce: true,
-      })
+    })
 
-     
 
-    return ( 
+
+    return (
         <div className="customer-exp">
-         
-                
-            <div ref={ref} className={ inView  ? "animation" : 'customer-exp-container container' }>
+
+
+            <div ref={ref} className={inView ? "animation" : "customer-exp-container container"}>
                 <div className="customer-exp-container__text">
                     <div className="customer-exp-container__title">
                         <h2>The best customer experiences are built with Zendesk</h2>
@@ -25,12 +25,12 @@ const CustomerExperience = () => {
                 </div>
 
                 <div className="customer-exp-container__img">
-                <img src="https://web-assets.zendesk.com/images/p-home/redesign-2019/customer-experience/photo-2-2x.jpg" alt="" className="female"/>
-                    <img src="https://web-assets.zendesk.com/images/p-home/redesign-2019/customer-experience/photo-1-2x.jpg" alt="" className="female-1"/>
-                    <img src="https://web-assets.zendesk.com/images/p-home/redesign-2019/customer-experience/photo-3-2x.jpg" alt="" className="male"/>
+                    <img src="https://web-assets.zendesk.com/images/p-home/redesign-2019/customer-experience/photo-2-2x.jpg" alt="" className="female" />
+                    <img src="https://web-assets.zendesk.com/images/p-home/redesign-2019/customer-experience/photo-1-2x.jpg" alt="" className="female-1" />
+                    <img src="https://web-assets.zendesk.com/images/p-home/redesign-2019/customer-experience/photo-3-2x.jpg" alt="" className="male" />
                 </div>
-        </div>
-           
+            </div>
+
         </div>
     )
 }
